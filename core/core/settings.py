188ebd8a2815289794d9b9d53077d0e561627d71
mahziar <ahmadian.mahziyar@gmail.com>
2024-05-08 16:53:09 +0330

    change project structure to standard DRF .

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7opmk4_fwyelfc2d-nz^uk_yiao5x=6e0axz+_kzsq&ab!p#zj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- core/todo/api/v1/urls.py
+++ core/todo/api/v1/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path, include
 from .views import TaskModelViewSet
 
-GetAllTask = TaskModelViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-GetOne = TaskModelViewSet.as_view(
-    {
+urlpatterns = [
+    path('task/', TaskModelViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+    }), name="task list"),
+
+    path('task/<int:pk>/', TaskModelViewSet.as_view({
         'put': 'update',
         'patch': 'partial_update',
         'get': 'retrieve',
         'delete': 'destroy',
-    }
-)
-urlpatterns = [
-
-    path('get/', GetAllTask, name="task list"),
-    path('get/<int:pk>/', GetOne, name="task get one "),
-
+    }),
+         name="detail"),
 ]
