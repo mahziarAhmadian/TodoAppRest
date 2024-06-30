c128445e89bfda3a33b19409219c67fdf4924f8d
Mahziar <93269144+mahziarAhmadian@users.noreply.github.com>
2024-06-30 11:01:10 +0330

    change secret key in setting .

--- core/core/settings.py
+++ core/core/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY' , default="test")
+SECRET_KEY = os.environ.get('SECRET_KEY' , default="sdsdagsdg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
