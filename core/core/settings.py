e4803481c612288412f1945437d14ae2e4159fa4
Mahziar <93269144+mahziarAhmadian@users.noreply.github.com>
2024-07-08 11:50:31 +0330

    test commit for testing cd workflow.

--- core/core/settings.py
+++ core/core/settings.py
@@ -195,7 +195,6 @@ CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
-
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
